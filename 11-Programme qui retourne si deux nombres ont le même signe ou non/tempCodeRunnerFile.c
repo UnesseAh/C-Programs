9483155aefddb359785616ad3c7655cf51995ff0
UnesseAh <youness.ahasla@gmail.com>
2022-11-12 14:43:32 +0100

    remove the .exe extension from all folders

--- 11-Programme qui retourne si deux nombres ont le même signe ou non/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-    // if(A > 0 && B > 0){
-    //     printf("They have the same sign!");
-    // }else if(A < 0 && B < 0){
-    //     printf("They have the same sign!");
-    // }else {
-    //     printf("They don't have the same sign!");
-    // }
